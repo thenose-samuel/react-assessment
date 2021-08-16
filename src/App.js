33c39c31383c080126de373ead512881b1187e68
thenose-samuel <skthaw1@gmail.com>
2021-08-16 23:14:12 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="Home Page">
-      <h1 class="text-4xl font-bold text-gray-900 p-4 border-b">its12 Giftworld</h1>
-      <ul>
-        <li></li>
-      </ul>
+    <div>
+      <nav class="bg-white flex justify-between shadow-lg  px-5 py-5">
+        <div>
+          <a href="#">
+            <span class="text-gray-800 text-3xl font-bold">
+              Its12 Giftworld
+            </span>
+          </a>
+        </div>
+        <div class="space-x-10 text-gray-500 font-bold">
+          <a href="#" class="text-gray-500 hover:text-gray-900">
+            Home
+          </a>
+          <a href="#" class="text-gray-500 hover:text-gray-900">
+            Categories
+          </a>
+          <a href="#" class="text-gray-500 hover:text-gray-900">
+            Products
+          </a>
+          <a href="#" class="text-gray-500 hover:text-gray-900">
+            Account
+          </a>
+        </div>
+      </nav>
+      <main>
+        <div class="place-content-center" className="Main content">
+          <span class="mx-10 my-10 text-gray-500">This is the home page!</span>
+        </div>
+      </main>
     </div>
   );
 };
