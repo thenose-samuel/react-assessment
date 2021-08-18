a26d724cb8f04331812394d380d733eb4fb00035
thenose-samuel <skthaw1@gmail.com>
2021-08-18 22:04:24 +0530

    completed second task

--- src/Pages/Pincode/index.js
+++ src/Pages/Pincode/index.js
@@ -1,13 +1,54 @@
 import React from "react";
 
-const PincodePage= () => {
-  return (
-    <div>
+class Location extends React.Component {
+  render() {
+    return (
       <div>
-        <span class="mx-10 my-10 text-gray-500">This is the Pin Code Checking page</span>
+        <h1>{this.props.blockName}</h1>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+class PincodePage extends React.Component {
+  constructor() {
+    super();
+    this.state = { address: ["Enter a pincode to check location"] };
+    this.getLocation = this.getLocation.bind(this);
+  }
+  getLocation(event) {
+    event.preventDefault();
+    const form = document.forms.locationFromPin;
+    const pincode = form.Pincode.value;
+    const block = [];
+    fetch(`https://api.postalpincode.in/pincode/${pincode}`)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        block.push(jsonData[0].PostOffice[0].Block)
+        this.setState({ address: block });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  render() {
+    const location = this.state.address.map((block) => (
+      <Location blockName={block}/>
+    ));
+    return (
+      <div>
+        <div>
+          <span class="mx-10 my-10 text-gray-500">
+            This is the Pin Code Checking page
+          </span>
+          <form name="locationFromPin" onSubmit={this.getLocation}>
+            <input type="text" name="Pincode" placeholder="Pincode" />
+            <button>Add</button>
+          </form>
+          <div>{location}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PincodePage;
