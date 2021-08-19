2d98d9a5d0ee69c27911bf68fe3024d37f4bc84b
thenose-samuel <skthaw1@gmail.com>
2021-08-19 12:19:03 +0530

    Styled the pin code page

--- src/Pages/Pincode/index.js
+++ src/Pages/Pincode/index.js
@@ -16,14 +16,20 @@ class PincodePage extends React.Component {
     this.getLocation = this.getLocation.bind(this);
   }
   getLocation(event) {
+    const block = [];
     event.preventDefault();
     const form = document.forms.locationFromPin;
     const pincode = form.Pincode.value;
-    const block = [];
+    //Pincode must be of length 6
+    if (pincode.length != 6) {
+      block.push("Invalid code");
+      this.setState({ address: block });
+      return;
+    }
     fetch(`https://api.postalpincode.in/pincode/${pincode}`)
       .then((response) => response.json())
       .then((jsonData) => {
-        block.push(jsonData[0].PostOffice[0].Block)
+        block.push(jsonData[0].PostOffice[0].Block);
         this.setState({ address: block });
       })
       .catch((error) => {
@@ -32,20 +38,22 @@ class PincodePage extends React.Component {
   }
   render() {
     const location = this.state.address.map((block) => (
-      <Location blockName={block}/>
+      <Location blockName={block} />
     ));
     return (
-      <div>
-        <div>
-          <span class="mx-10 my-10 text-gray-500">
-            This is the Pin Code Checking page
-          </span>
+      <div class="flex justify-around items-center h-screen w-full">
+        <div class="shadow-2xl p-10 rounded-lg">
           <form name="locationFromPin" onSubmit={this.getLocation}>
-            <input type="text" name="Pincode" placeholder="Pincode" />
-            <button>Add</button>
-          </form>
-          <div>{location}</div>
+          <div  class="flex flex-col mb-4">
+            <label class="mb-2 uppercase font-bold text-lg text-gray-600">Enter your pin</label>
+            <input class="border py-2 px-3" type="text" name="Pincode" placeholder="Pincode" />
+            <button class="block bg-gray-400 hover:bg-gray-800 text-black uppercase text-sm mx-auto m-2 p-3 rounded">Search</button>
+            </div>
+            <div>
+          <span>{location}</span>
         </div>
+          </form>
+          </div>
       </div>
     );
   }
